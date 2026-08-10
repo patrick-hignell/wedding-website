676cc52368e0fb4c0310e1f78912be3620c76929
Patrick Hignell <patrickhignell@hotmail.com>
2026-08-10 17:22:01 +1200

    add registry edit

--- client/components/RegistryAdmin.tsx
+++ client/components/RegistryAdmin.tsx
@@ -10,6 +10,8 @@ export default function RegistryAdmin() {
     location: '',
     bio: '',
     cost: 0,
+    link: '',
+    image: '',
   })
   const [registryEditItem, setRegistryEditItem] = useState<Registry>({
     id: 0,
@@ -17,6 +19,8 @@ export default function RegistryAdmin() {
     location: '',
     bio: '',
     cost: 0,
+    link: '',
+    image: '',
   })
   const {
     data: registry,
@@ -25,11 +29,20 @@ export default function RegistryAdmin() {
     error,
     delete: deleteRegistry,
     add: addRegistry,
-    // edit: editRegistry,
+    edit: editRegistry,
   } = useRegistry()
 
   function handleDeleteRegistry(registryItem: Registry) {
     deleteRegistry.mutate(registryItem)
+    setRegistryEditItem({
+      id: 0,
+      name: '',
+      location: '',
+      bio: '',
+      cost: 0,
+      link: '',
+      image: '',
+    })
   }
 
   function handleRegistryItemDataChange(
@@ -42,9 +55,34 @@ export default function RegistryAdmin() {
     }))
   }
 
+  function handleRegistryEditItemChange(
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) {
+    const { name, value, type } = e.target
+    setRegistryEditItem((prev) => ({
+      ...prev,
+      [name]: type === 'number' ? Number(value) : value,
+    }))
+  }
+
   function handleRegistryItemAdd() {
     if (registryItemData.name.length != 0 && !isNaN(registryItemData.cost)) {
       addRegistry.mutate(registryItemData)
+    }
+  }
+
+  function handleRegistryItemEdit() {
+    if (registryEditItem.name.length != 0 && !isNaN(registryEditItem.cost)) {
+      editRegistry.mutate(registryEditItem)
+      setRegistryEditItem({
+        id: 0,
+        name: '',
+        location: '',
+        bio: '',
+        cost: 0,
+        link: '',
+        image: '',
+      })
     }
   }
 
@@ -84,32 +122,106 @@ export default function RegistryAdmin() {
         </thead>
         <tbody>
           {registry.map((registryItem) => (
-            <tr key={registryItem.id} className="alternating">
-              <td className="cell">{registryItem.id}</td>
-              <td className="cell">{registryItem.name}</td>
-              <td className="cell">{registryItem.location}</td>
-              <td className="cell">{registryItem.bio}</td>
-              <td className="cell">{registryItem.cost}</td>
-              <td className="cell break-all">{registryItem.link}</td>
-              <td className="cell">
-                <img
-                  className=""
-                  alt={registryItem.image}
-                  src={`/images/${registryItem.image}.png`}
-                  style={{ width: 400, height: 'auto' }}
-                />
-              </td>
-              <td className="cell text-center">
-                <button onClick={() => handleDeleteRegistry(registryItem)}>
-                  <i className="bi bi-pencil-fill"></i>
-                </button>
-              </td>
-              <td className="cell text-center">
-                <button onClick={() => handleDeleteRegistry(registryItem)}>
-                  <i className="bi bi-x-circle-fill"></i>
-                </button>
-              </td>
-            </tr>
+            <>
+              {registryItem.id != registryEditItem.id && (
+                <tr key={registryItem.id} className="alternating">
+                  <td className="cell">{registryItem.id}</td>
+                  <td className="cell">{registryItem.name}</td>
+                  <td className="cell">{registryItem.location}</td>
+                  <td className="cell">{registryItem.bio}</td>
+                  <td className="cell">{registryItem.cost}</td>
+                  <td className="cell break-all">{registryItem.link}</td>
+                  <td className="cell">
+                    <img
+                      className=""
+                      alt={registryItem.image}
+                      src={`/images/${registryItem.image}.png`}
+                      style={{ width: 400, height: 'auto' }}
+                    />
+                  </td>
+                  <td className="cell text-center">
+                    <button onClick={() => handleEditSelect(registryItem.id)}>
+                      <i className="bi bi-pencil-fill"></i>
+                    </button>
+                  </td>
+                  <td className="cell text-center">
+                    <button onClick={() => handleDeleteRegistry(registryItem)}>
+                      <i className="bi bi-x-circle-fill"></i>
+                    </button>
+                  </td>
+                </tr>
+              )}
+              {registryItem.id == registryEditItem.id && (
+                <tr key={registryItem.id} className="alternating">
+                  <td className="cell">{registryItem.id}</td>
+                  <td>
+                    <input
+                      name="name"
+                      className="h-10 w-full justify-center rounded border border-black pl-3"
+                      type="text"
+                      value={registryEditItem.name}
+                      onChange={handleRegistryEditItemChange}
+                    ></input>
+                  </td>
+                  <td>
+                    <input
+                      name="location"
+                      className="h-10 w-full justify-center rounded border border-black pl-3"
+                      type="text"
+                      value={registryEditItem.location}
+                      onChange={handleRegistryEditItemChange}
+                    ></input>
+                  </td>
+                  <td>
+                    <input
+                      name="bio"
+                      className="h-10 w-full justify-center rounded border border-black pl-3"
+                      type="text"
+                      value={registryEditItem.bio}
+                      onChange={handleRegistryEditItemChange}
+                    ></input>
+                  </td>
+                  <td>
+                    <input
+                      name="cost"
+                      className="h-10 w-full justify-center rounded border border-black pl-3"
+                      type="text"
+                      inputMode="decimal"
+                      value={registryEditItem.cost}
+                      onChange={handleRegistryEditItemChange}
+                    ></input>
+                  </td>
+                  <td>
+                    <input
+                      name="link"
+                      className="h-10 w-full justify-center rounded border border-black pl-3"
+                      type="text"
+                      value={registryEditItem.link}
+                      onChange={handleRegistryEditItemChange}
+                    ></input>
+                  </td>
+                  <td>
+                    <input
+                      name="image"
+                      className="h-10 w-full justify-center rounded border border-black pl-3"
+                      type="text"
+                      value={registryEditItem.image}
+                      onChange={handleRegistryEditItemChange}
+                    ></input>
+                  </td>
+                  <td className="cell text-center">
+                    <button onClick={handleRegistryItemEdit}>
+                      <i className="bi bi-pencil-fill"></i>
+                    </button>
+                  </td>
+                  <td className="cell text-center">
+                    <button onClick={() => handleDeleteRegistry(registryItem)}>
+                      <i className="bi bi-x-circle-fill"></i>
+                    </button>
+                  </td>
+                </tr>
+              )}
+            </>
           ))}
           <tr>
             <td></td>
@@ -118,6 +230,7 @@ export default function RegistryAdmin() {
                 name="name"
                 className="h-10 w-full justify-center rounded border border-black pl-3"
                 type="text"
+                value={registryItemData.name}
                 onChange={handleRegistryItemDataChange}
               ></input>
             </td>
@@ -126,6 +239,7 @@ export default function RegistryAdmin() {
                 name="location"
                 className="h-10 w-full justify-center rounded border border-black pl-3"
                 type="text"
+                value={registryItemData.location}
                 onChange={handleRegistryItemDataChange}
               ></input>
             </td>
@@ -134,6 +248,7 @@ export default function RegistryAdmin() {
                 name="bio"
                 className="h-10 w-full justify-center rounded border border-black pl-3"
                 type="text"
+                value={registryItemData.bio}
                 onChange={handleRegistryItemDataChange}
               ></input>
             </td>
@@ -143,6 +258,7 @@ export default function RegistryAdmin() {
                 className="h-10 w-full justify-center rounded border border-black pl-3"
                 type="text"
                 inputMode="decimal"
+                value={registryItemData.cost}
                 onChange={handleRegistryItemDataChange}
               ></input>
             </td>
@@ -151,6 +267,7 @@ export default function RegistryAdmin() {
                 name="link"
                 className="h-10 w-full justify-center rounded border border-black pl-3"
                 type="text"
+                value={registryItemData.link}
                 onChange={handleRegistryItemDataChange}
               ></input>
             </td>
@@ -159,6 +276,7 @@ export default function RegistryAdmin() {
                 name="image"
                 className="h-10 w-full justify-center rounded border border-black pl-3"
                 type="text"
+                value={registryItemData.image}
                 onChange={handleRegistryItemDataChange}
               ></input>
             </td>
